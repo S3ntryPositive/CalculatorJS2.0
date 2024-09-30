76bb4897513049728f9e0b51450df2d5e5b8f941
S3ntry_Positive <89636989+S3ntryPositive@users.noreply.github.com>
2024-09-30 19:41:56 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,18 @@
+
 window.addEventListener("load", addListener)
 
 var number1, number2, result;
 
 function addListener()
 {
-	document.getElementById("InputResult").disabled = true;
+	document.getElementById("InputResult").disabled = false;
+    document.getElementById("btnPeriod").addEventListener("click", Period);
+    document.getElementById("btnFactorial").addEventListener("click", Factorial);
+    document.getElementById("btnPlus").addEventListener("click", Add);
+    document.getElementById("btnDivide").addEventListener("click", Divide);
+    document.getElementById("btnMultiply").addEventListener("click", Multiply);
+    document.getElementById("btnMinus").addEventListener("click", Subtract);
+    document.getElementById("btnPower").addEventListener("click", Power);
 	document.getElementById("btn1").addEventListener("click", One);
 	document.getElementById("btn2").addEventListener("click", Two);
 	document.getElementById("btn3").addEventListener("click", Three);
@@ -16,7 +24,7 @@ function addListener()
 	document.getElementById("btn9").addEventListener("click", Nine);
 	document.getElementById("btn0").addEventListener("click", Zero);
 	document.getElementById("btnClear").addEventListener("click", ClearInput);
-	
+    document.getElementById("btnEqual").addEventListener("click", Solve);
 }
 
 function One()
@@ -76,5 +84,42 @@ function ClearInput()
 
 function Add()
 {
-	
+	document.getElementById("InputResult").value += "+";
+}
+
+function Divide()
+{
+	document.getElementById("InputResult").value += "/";
+}
+
+function Multiply()
+{
+	document.getElementById("InputResult").value += "*";
+}
+
+function Subtract()
+{
+	document.getElementById("InputResult").value += "-";
+}
+
+function Power()
+{
+	document.getElementById("InputResult").value += "^";
+}
+
+function Period()
+{
+	document.getElementById("InputResult").value += ".";
+}
+
+function Factorial()
+{
+    document.getElementById("InputResult").value += "!";
+}
+
+function Solve()
+{
+    var Equation = document.getElementById("InputResult").value;
+    var Result = math.evaluate(Equation);
+    document.getElementById("InputResult").value = Result;
 }
